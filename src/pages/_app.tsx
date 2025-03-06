3b8e2811161b7a10a6396c73994f27802506db15
Jm <jmdelasalas.dev@gmail.com>
2025-03-06 21:10:03 +0800

    Installed needed packages and wrapped the root to the new theme provider and layout

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,17 +1,20 @@
-import { ThemeProvider } from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
-import theme from '../theme/theme';
+import { ThemeProvider } from '@/theme/theme';
 import type { AppProps } from 'next/app';
 import { Nunito_Sans } from 'next/font/google';
+
+import Layout from '@/components/Layout/Layout';
 
 const nunitoSans = Nunito_Sans({ subsets: ['latin'], variable: '--font-nunito-sans' });
 
 export default function App({ Component, pageProps }: AppProps) {
 	return (
 		<main className={`${nunitoSans.variable} font-sans`}>
-			<ThemeProvider theme={theme}>
+			<ThemeProvider>
 				<CssBaseline />
-				<Component {...pageProps} />
+				<Layout>
+					<Component {...pageProps} />
+				</Layout>
 			</ThemeProvider>
 		</main>
 	);
